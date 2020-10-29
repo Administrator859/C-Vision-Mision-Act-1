9a9a3442752e93c186b390313cb338fdb611813e
Administrator <65272329+Administrator859@users.noreply.github.com>
2020-10-29 23:31:48 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CPROJECTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("");
+            Console.WriteLine("");
+             Console.WriteLine("");
+            Console.WriteLine("                                             **||VISION-MISSION STATEMENT||**");
+              Console.WriteLine("                                               ----------------------------");
+               Console.WriteLine("");
+            Console.WriteLine("                                             -----------||VISION||-----------");
+             Console.WriteLine("");
+               Console.WriteLine("    Notre Dame of Cotabato is a Marist, Filipino, Catholic, and Inter-faith Institution that envisions persons who are");
+            Console.WriteLine("       God-centered evangelizers, transformative individuals, morally upright, academically and globally competent.");
+                Console.WriteLine("");
+            Console.WriteLine("                                             -----------||MISSION||-----------");
+              Console.WriteLine("");
+             Console.WriteLine("      As a Catholic school, it animates the Gospel values, through faith formation, integral development of persons,");
+            Console.WriteLine("                     transformative intercultural dialogue, justice, peace and care for the earth.");
+              Console.WriteLine("");
+            Console.WriteLine("        As a Marist school, it promotes Marian virtues and H.E.A.R.T (Humane, Excellence, Adaptive, Responsive, and");
+               Console.WriteLine(" Transformative) core values. Inspired by the charism of St. Marcellin Champagnat, it develops its personnel, parents and");
+            Console.WriteLine("alumni in carrying out its mission in protecting the children’s right and in serving the youth especially the least favored.");
+                Console.WriteLine("");
+            Console.WriteLine("         As a Filipino school, it takes part in the attainment of the goals of education embodied in the Philippine");
+             Console.WriteLine("    Constitution and in the Basic Education Curriculum. It preserves and cultivates Filipino values and collective dreams");
+            Console.WriteLine("                                      through responsible and transformative deeds.");
+              Console.WriteLine("");
+            Console.WriteLine("");
+        Console.WriteLine("");
+        }
+    }
+}
